e35cd4d0d923cf880aee0429bbcb5465855d7053
xiaojuzi <1206085316@qq.com>
2020-12-17 23:20:21 +0800

    update commit

--- vhr-develop/vhr/vhrserver/vhr-service/src/main/java/org/javaboy/vhr/utils/POIUtils.java
+++ vhr-develop/vhr/vhrserver/vhr-service/src/main/java/org/javaboy/vhr/utils/POIUtils.java
@@ -20,13 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @作者 江南一点雨
- * @公众号 江南一点雨
- * @微信号 a_java_boy
- * @GitHub https://github.com/lenve
- * @博客 http://wangsong.blog.csdn.net
- * @网站 http://www.javaboy.org
- * @时间 2019-11-11 23:25
+xiaojuzi
  */
 public class POIUtils {
 
