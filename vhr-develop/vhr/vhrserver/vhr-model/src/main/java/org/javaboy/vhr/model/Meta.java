82c42a9547c4004a4ebce97d831811ce2e34763d
xiaojuzibushuai <57610201+xiaojuzibushuai@users.noreply.github.com>
2020-12-17 17:12:10 +0800

    first commit

---
+++ vhr-develop/vhr/vhrserver/vhr-model/src/main/java/org/javaboy/vhr/model/Meta.java
@@ -0,0 +1,28 @@
+package org.javaboy.vhr.model;
+
+import java.io.Serializable;
+
+/**
+ xiaojuzi
+ */
+public class Meta implements Serializable {
+    private Boolean keepAlive;
+
+    private Boolean requireAuth;
+
+    public Boolean getKeepAlive() {
+        return keepAlive;
+    }
+
+    public void setKeepAlive(Boolean keepAlive) {
+        this.keepAlive = keepAlive;
+    }
+
+    public Boolean getRequireAuth() {
+        return requireAuth;
+    }
+
+    public void setRequireAuth(Boolean requireAuth) {
+        this.requireAuth = requireAuth;
+    }
+}
---
+++ vhr-develop/vuehr/src/views/per/PerEc.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        员工惩罚
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PerEc"
+    }
+</script>
+
+<style scoped>
+
+</style>
